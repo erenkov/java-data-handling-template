66401ca98afe5841b80ca11e3ab226ffd3a08b5f
aleksandr.erenkov <al.erenkov@yandex.ru>
2020-04-12 21:23:11 +0400

    + SimpleFileRep v.0.1

--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -1,5 +1,11 @@
 package com.epam.izh.rd.online.repository;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class SimpleFileRepository implements FileRepository {
 
     /**
@@ -10,7 +16,18 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countFilesInDirectory(String path) {
-        return 0;
+        File file = new File("src/main/resources/" + path);
+        long count = 0;
+        File[] files = file.listFiles();
+        for (File tempFile : files != null ? files : new File[0]) {     //Если listFiles вернул null - подменяем на пустой массив
+            if (tempFile.isDirectory()) {
+                count += countFilesInDirectory(path + File.separator + tempFile.getName());
+            } else {
+                count++;
+
+            }
+        }
+        return count;
     }
 
     /**
@@ -21,7 +38,18 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countDirsInDirectory(String path) {
-        return 0;
+        File file = new File("src/main/resources/" + path);
+        long count = 0;
+        if(file.isDirectory()){
+            count++;
+        }
+        File[] files = file.listFiles();
+        for (File tempFile : files != null ? files : new File[0]) {     //Если listFiles вернул null - подменяем на пустой массив
+            if (tempFile.isDirectory()) {
+                count += countDirsInDirectory(path + File.separator + tempFile.getName());
+            }
+        }
+        return count;
     }
 
     /**
@@ -32,7 +60,13 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public void copyTXTFiles(String from, String to) {
-        return;
+        Path originalPath = Paths.get(from);
+        Path copied = Paths.get(to);
+        try {
+            Files.copy(originalPath, copied, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -44,6 +78,15 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public boolean createFile(String path, String name) {
+        File newPath = new File(path);
+        File newFile = new File(path + "/" + name);
+        try {
+            if (newPath.mkdir()) {
+                return newFile.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
@@ -55,6 +98,12 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public String readFileFromResources(String fileName) {
+        /* т.к. возвращаем одну строку, а не массив строк, то и считываю далее из файла только одну строку */
+        try (BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/" + fileName))) {
+            return bufReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
